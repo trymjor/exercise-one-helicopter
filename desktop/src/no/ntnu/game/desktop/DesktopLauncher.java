ea17f1d831b21ea5a7139400fb1d391ac6b6f6c8
Trym <trymj@stud.ntnu.no>
2022-01-31 10:32:23 +0100

    Initial libgdx commit

---
+++ desktop/src/no/ntnu/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.ntnu.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.ntnu.game.helipeli;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new helipeli(), config);
+	}
+}
---
+++ ios/src/no/ntnu/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package no.ntnu.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import no.ntnu.game.helipeli;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new helipeli(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
